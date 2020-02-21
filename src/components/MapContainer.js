5f92497297e402e3bac91b30534959f4a124d7e7
Inna <inna.ivashko@gmail.com>
2020-02-21 12:54:55 +0100

    added infoWindow  onClick to Map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
     render() {
         return (
             <Map
             google={this.props.google}
             zoom={15}
-            // style={mapStyles}
             initialCenter={{
              lat: 51.443686,
              lng: 5.479193
             }}
-          />
+            >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Beauty Art'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+        <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+        </div>
+        </InfoWindow>
+            </Map>
             )
     }
 }
