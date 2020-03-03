4a60076b788015a96a4d886435f7163de125c0ea
Inna <inna.ivashko@gmail.com>
2020-03-03 21:59:21 +0100

    cleaned up for back end

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {router as lalaRouter} from './routes/lala';
+// import {router as lalaRouter} from './routes/lala';
 import {router as appointmentRouter} from './routes/appointment';
 import express from 'express';
 import path from 'path';
@@ -8,7 +8,6 @@ const publicPath = path.join(__dirname, '../build');
 
 const app = express();
 app.use(express.static(publicPath));
-app.use('/lala', lalaRouter);
 app.use('/appointment', appointmentRouter);
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
--- backend/routes/appointment.js
+++ backend/routes/appointment.js
@@ -3,35 +3,3 @@ export const router = express.Router();
 
 let Appointment = require('../models/appointment.model');
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//     res.json({ username: 'Flavio' });
-// });
-
-router.get('/', (req, res) => {
-    Appointment.find()
-      .then(appointments => res.json(appointments))
-      .catch(err => res.status(400).json('Error: ' + err));
-  });
-
-// router.get('/minutes/create', minutes_controller.minutesCreateGet);
-// export const minutesCreateGet = (req, res, next) => {
-//     async.parallel({
-//       attendees: callback => Attendee.find(callback),
-//       counts: viewCounts
-//     }, (err, results) => {
-//       if (err)
-//         return next(err);
-//       res.render('minutes_form', { title: 'Create Minutes',
-//                                    attendee_list: results.attendees,
-//                                    counts: results.counts});
-//       });
-//   };
-// router.post('/minutes/create', minutes_controller.minutesCreatePost);
-
-
-// router.route('/:id').get((req, res) => {
-//     Exercise.findById(req.params.id)
-//       .then(exercise => res.json(exercise))
-//       .catch(err => res.status(400).json('Error: ' + err));
-//   });
